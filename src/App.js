0427e9441f91aa2d50c8a88590bac42d60c1dae5
ChernyshevaNetology <chernysheva87@mail.ru>
2020-10-15 11:20:53 +0200

    fix: changed app.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,29 @@
 import React from "react";
-import ShopItemFunc from './components/ShopItemFunc'
+import ShopItemFunc from "./components/ShopItemFunc";
 import "./css/main.css";
 
 function App() {
-
   const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+    brand: "Tiger of Sweden",
+    title: "Leonard coat",
+    description: "Minimalistic coat in cotton-blend",
+    descriptionFull:
+      "Men's minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.",
     price: 399,
-    currency: '£'
-  }  
-  
-return (
-  <div className="container">
-    <div className="background-element">
+    currency: "£",
+  };
+
+  return (
+    <div className="container">
+      <div className="background-element"></div>
+      <div className="highlight-window">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="window">
+        <ShopItemFunc item={item} />
+      </div>
     </div>
-    <div className="highlight-window">
-      <div className='highlight-overlay'></div>
-    </div>
-    <div className="window">
-      <ShopItemFunc item={item} />
-    </div>
-  </div>
-)
+  );
 }
 
-
-
 export default App;
-
-// This is a demo task.
-
-// Write a function:
-
-// function solution(A);
-
-// that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
-
-// For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
-
-// Given A = [1, 2, 3], the function should return 4.
-
-// Given A = [−1, −3], the function should return 1.
-
-// Write an efficient algorithm for the following assumptions:
-
-// N is an integer within the range [1..100,000];
-// each element of array A is an integer within the range [−1,000,000..1,000,000].
-// Copyright 2009–2020 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
